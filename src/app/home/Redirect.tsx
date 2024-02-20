8b1137ebe479ba60b0f0c8eb1be2c30e2e495214
fanyinghao <fanyinghao@gmail.com>
2024-02-20 15:11:26 +0000

    ✨ feat: set configs from url param ✨ auto load settings #917

--- src/app/home/Redirect.tsx
+++ src/app/home/Redirect.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { memo, useEffect } from 'react';
 
 import { messageService } from '@/services/message';
 import { sessionService } from '@/services/session';
+import { useGlobalStore } from '@/store/global';
 import { useSessionStore } from '@/store/session';
 
 const checkHasConversation = async () => {
@@ -17,6 +18,11 @@ const Redirect = memo(() => {
   const router = useRouter();
   const [switchSession] = useSessionStore((s) => [s.switchSession]);
 
+  // get settings str from query
+  const searchParams = useSearchParams();
+  const settings = searchParams.get('settings');
+  const [setConfig] = useGlobalStore((s) => [s.setModelProviderConfig]);
+
   useEffect(() => {
     checkHasConversation().then((hasData) => {
       if (hasData) {
@@ -28,6 +34,25 @@ const Redirect = memo(() => {
       }
     });
   }, []);
+
+  useEffect(() => {
+    if (settings) {
+      try {
+        const json = JSON.parse(settings);
+        const { openAI } = json as {
+          openAI: {
+            OPENAI_API_KEY?: string;
+            endpoint?: string;
+          };
+        };
+        setTimeout(() => {
+          if (openAI) setConfig('openAI', openAI);
+        }, 5000);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }, [settings]);
 
   return null;
 });
