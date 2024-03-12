fa657833e1d3b06017037e67b0881365517595dd
fanyinghao <fanyinghao@gmail.com>
2024-03-12 18:36:36 +0000

    ✨ feat: change page title

--- src/components/FullscreenLoading/index.tsx
+++ src/components/FullscreenLoading/index.tsx
@@ -7,7 +7,7 @@ const FullscreenLoading = memo<{ title?: string }>(({ title }) => {
   return (
     <Flexbox height={'100%'} style={{ userSelect: 'none' }} width={'100%'}>
       <Center flex={1} gap={12} width={'100%'}>
-        <Logo extra={<span>Wecode Chat</span>} size={48} type={'high-contrast'} />
+        <Logo extra={<span>Wecode Chat</span>} size={48} type={'3d'} />
         <Center gap={16} horizontal>
           <Icon icon={Loader2} spin />
           {title}
